bc20315b9e3504bd956ee2ef4b6fb82301d14b22
Jason Berlinsky <jason@barefootcoders.com>
2014-05-30 21:30:47 -0400

    ARRRRGH

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -1,5 +1,5 @@
 node[:nginx][:apps].each do |app_name, app_attributes|
-  nginx_app app_name do
+  app app_name do
     app_attributes.each do |attribute, value|
       instance_variable_set("@#{attribute}", value)
     end
